f65dbc828cfda61a2d26fa05a9ce75dfcd56ed97
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2025-07-09 22:22:09 -0600

    ip update

--- src/core/config/consts.ts
+++ src/core/config/consts.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   import.meta.env.MODE === 'production'
     ? '/api'
-    : 'http://34.200.107.148:3000/api';
+    : 'http://20.245.212.209:3000/api';
 export const REGISTER_STUDENT_URL = "/auth/register-student";
 export const STUDENTS_LIST_URL = "/auth/students";
 export const STORAGE_KEY = "registered_students";
